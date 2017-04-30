379c934953d52b2faf5a37abfb89ed48cfbd3d20
yuta naito <yuta.n@yutanoMacBook-Pro.local>
2017-05-01 01:04:36 +0900

    sample 3.2  - add function to get weekday from date

--- wsgiSample01.py
+++ wsgiSample01.py
@@ -1,46 +1,40 @@
 # -*- coding: utf-8 -*-
 from wsgiref import simple_server
 from cgi import parse_qs
+import calendar
 
 def application(env, res):
     status = "200 OK"
     get_info = parse_qs(env['QUERY_STRING'])
-    body = 'year=' + get_info['year'][0] + '<br>'
-    body += 'month=' + get_info['month'][0] + '<br>'
-    body += 'day=' + get_info['day'][0] + '<br>'
-    #body = makeString()
+    year = get_info['year'][0]
+    month = get_info['month'][0]
+    day = get_info['day'][0]
+
+    weekdayStr = getWeekDay(year,month,day)
+
+    body = '%s年%s月%s日は%s曜日です' % (year,month,day,weekdayStr)
+
     header =  [("Content-type", "text/html;charset=UTF-8"),("Content-Length",str(len(body)))]
     res(status,header)
     return [body]
 
-def makeString():
-
-    discription = """
-    <html><body>
-        <table width='300' border='1'>
-            <caption>テストテーブル</caption>
-            <tr>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-            </tr>
-            <tr>
-                <th>4</th>
-                <th>5</th>
-                <th>6</th>
-            </tr>
-            <tr>
-                <th>7</th>
-                <th>8</th>
-                <th>9</th>
-            </tr>
-        </table>
-    </body></html>
-
-    """
-    #body = "Hello wsgi application\n"
-    print discription
-    return discription
+def getWeekDay(year,month,day):
+    weekdayStr = ""
+    weekday = calendar.weekday(int(year),int(month),int(day))
+    if weekday == 0:
+        return 'Monday'
+    elif weekday == 1:
+        return 'Thuesday'
+    elif weekday == 2:
+        return 'Wednesday'
+    elif weekday == 3:
+        return 'Thursday'
+    elif weekday == 4:
+        return 'Friday'
+    elif weekday == 5:
+        return 'Satuaday'
+    elif weekday == 6:
+        return 'Sunday'
 
 if __name__ == "__main__":
     sv = simple_server.make_server("", 8080, application) #host , port , application
