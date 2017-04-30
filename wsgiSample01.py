c44898f74a4450012cfba1cb26eb0e5f03c3799c
yuta naito <yuta.n@yutanoMacBook-Pro.local>
2017-04-30 23:10:59 +0900

    fix to show in UTF-8

--- wsgiSample01.py
+++ wsgiSample01.py
@@ -1,43 +1,41 @@
+# -*- coding: utf-8 -*-
 from wsgiref import simple_server
 
 def application(env, res):
     status = "200 OK"
-    body = "Hello wsgi application\n"
-    header =  [("Content-type", "text/plain"),("Content-Length",str(len(body)))]
+    body = makeString()
+    header =  [("Content-type", "text/html;charset=UTF-8"),("Content-Length",str(len(body)))]
     res(status,header)
     return [body]
 
-def makeString(env, res):
-    htmlStartTag = "<html>"
-    htmlEndTag = "</html>"
-    bodyStartTag = "<body>"
-    bodyEndTag = "</body>"
+def makeString():
 
     discription = """
-    <table width = '300' border = '1'>
-        <caption>テストテーブル</caption>
-        <tr>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-        </tr>
-        <tr>
-            <th>4</th>
-            <th>5</th>
-            <th>6</th>
-        </tr>
-        <tr>
-            <th>7</th>
-            <th>8</th>
-            <th>9</th>
-        </tr>
-    </table>
+    <html><body>
+        <table width='300' border='1'>
+            <caption>テストテーブル</caption>
+            <tr>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+            </tr>
+            <tr>
+                <th>4</th>
+                <th>5</th>
+                <th>6</th>
+            </tr>
+            <tr>
+                <th>7</th>
+                <th>8</th>
+                <th>9</th>
+            </tr>
+        </table>
+    </body></html>
+
     """
     #body = "Hello wsgi application\n"
-    body =  htmlStartTag + bodyStartTag + discription + bodyEndTag + htmlEndTag
-    header =  [("Content-type", "text/plain"),("Content-Length",str(len(body)))]
-    res(status,header)
-    return [body]
+    print discription
+    return discription
 
 if __name__ == "__main__":
     sv = simple_server.make_server("", 8080, application) #host , port , application
